d08820b1b2ce5dca8e7af279e03a65caa2cfdd70
Anna Vitoria <vitoria.963369@gmail.com>
2025-09-29 08:37:20 -0300

    Implementa métodos para realizar e salvar vendas

--- database.py
+++ database.py
@@ -12,6 +12,24 @@ crs.execute('''
         preco_venda DOUBLE NOT NULL)''')
 conn.commit()
 
+crs.execute('''
+        CREATE TABLE IF NOT EXISTS Vendas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            produto_id INTEGER,
+            qtd_vendida INTEGER,
+            data TEXT,
+            )''')
+conn.commit()
+
 def cadastrar_podutos_db(nome, qtd, p_compra, p_venda):
     crs.execute("INSERT INTO Produtos (nome, qtd_estoque, preco_compra, preco_venda) VALUES (?, ?, ?, ?)", (nome, qtd, p_compra, p_venda))
+    conn.commit()
+
+def executar_venda(prod_id, qtd, data):
+    crs.execute('INSERT INTO Vendas(produto_id, qtd_vendida, data) VALUES (?, ?, ?)', (prod_id, qtd, data))
+    conn.commit()
+    qtd_old = crs.execute('SELECT qtd_estoque FROM Produtos WHERE id= ?', prod_id)
+    qtd_new = qtd_old.fetchone()[0]-qtd
+
+    crs.execute('UPDATE Produtos SET qtd_estoque= ? WHERE id= ?', (qtd_new, prod_id))
     conn.commit()
--- produtos.py
+++ produtos.py
@@ -3,3 +3,7 @@ class Produtos:
     def cadastrar_produtos(self, nome, qtd_estoque, preco_compra, preco_venda):
         db.cadastrar_podutos_db(nome=nome, qtd=qtd_estoque, p_compra=preco_compra, p_venda=preco_venda)
         return 'Produto Cadastrado'
+    
+    def vender(self, produto_id, qtd, data):
+        db.executar_venda(prod_id= produto_id, qtd= qtd, data= data)
+        return 'Venda realizada'
